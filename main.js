f21951aaa3534e3867dd5f616b47f25e1ce2b445
Thomas Morris <tomchrismorris@yahoo.com>
2022-07-26 17:19:04 -0400

    Clicking buttons displays text, need to code clear and equal

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 
 const colorChange = document.querySelectorAll(".hover");
 const fontChange = document.querySelectorAll(".light");
+const calcButton = document.querySelectorAll(".button");
+let console = document.querySelector(".result");
+
+calcButton.forEach(button => {
+    button.addEventListener("click", displayButton);
+})
+
+function displayButton(e) {
+    console.innerHTML += e.target.innerHTML;
+}
 
 fontChange.forEach(button => {
     button.addEventListener("mouseover", fontColorSwitch)
