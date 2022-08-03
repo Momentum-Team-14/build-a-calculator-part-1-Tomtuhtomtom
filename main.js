39fa694985084ed43698ea0790876988c5810bba
Thomas Morris <tomchrismorris@yahoo.com>
2022-08-03 18:25:33 -0400

    cleaned up some code and added lots of notes in prep for presentation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+//set variables to "grab" classes from HTML
 const colorChange = document.querySelectorAll(".hover");
 const fontChange = document.querySelectorAll(".light");
 const calcButton = document.querySelectorAll(".button");
@@ -6,28 +7,41 @@ const clearButton = document.querySelector(".buttonc");
 const equalButton = document.querySelector(".buttone");
 let displayConsole = document.querySelector(".result");
 
+// use variable from above with forEach function to add Event Listerner
+// to each button clicked and call function displayButton when clicked
 calcButton.forEach(button => {
     button.addEventListener("click", displayButton);
 })
 
+//displayButton function that was called above that 
+//uses the innerHTML property to change the display on the 
+//calculator to add what is written on the button(the innerHTML)
 function displayButton(e) {
-    if (displayConsole.innerHTML === "0" && e.target.innerHTML != "0") {
+    //if statement that gets rid of the zero at the beginning
+    if (displayConsole.innerHTML === "0" && e.target.innerHTML !=="0") {
         displayConsole.innerHTML = "";
-    } else if (displayConsole.innerHTML === "0" && e.target.innerHTML === "0")
-        displayConsole.innerHTML = "0";
+    }
+    //if statement to change X on the button to display the * operator
     if (e.target.innerHTML === "X") {
         displayConsole.innerHTML += "*";
     } else {
+        //for all else, add the character on the button to the display console
     displayConsole.innerHTML += e.target.innerHTML;
 }}
 
+// resets the display console to zero when clear button is pressed
 clearButton.addEventListener("click", function(){
     displayConsole.innerHTML = "0";
 })
 
+//runs the eval() function for the innerHTML inside the display console
+//to see if it can be solved like a math problem
 equalButton.addEventListener("click", function(){
     try {eval(displayConsole.innerHTML);
     } catch (e) {
+        // if the display console can't be solved like a math problem
+        // it displays a error message with the alert function and resets the 
+        // display console to zero
         if (e instanceof SyntaxError) {
             alert("SyntaxError");
             displayConsole.innerHTML = "0";
@@ -35,14 +49,12 @@ equalButton.addEventListener("click", function(){
         else {
             throw e;}
     }
+    // displays the answer in the console after being checked by the above if statement
     let answer = eval(displayConsole.innerHTML);
-    if (typeof answer != "number") {
-        displayConsole.innerHTML = "0";
-    } else {
     displayConsole.innerHTML = answer;
-    }
 })
 
+//Event listerners for the different hover effects
 fontChange.forEach(button => {
     button.addEventListener("mouseover", fontColorSwitch)
     button.addEventListener("mouseout", colorReset)
@@ -68,7 +80,8 @@ function fontColorSwitch(e) {
 }
 
 
-// Started messing with skins, very ugly code, just trying to get it to work
+// Started messing with skins to see if it works
+// very ugly code, just trying to get it to work
 
 const alternativeSkin = document.querySelector(".skin")
 const originalSkin = document.querySelector(".original")
